91cb3f11eb789c7a86cac547d62d9b5d1b65f240
Annika Gadman <annika.gadman@gmail.com>
2025-04-21 09:07:26 -0500

    ändrad config länk till publicerad webservice

--- js/config.js
+++ js/config.js
@@ -1,1 +1,2 @@
-const API_BASE_URL = "http://localhost:3000/api/workexperience"; 
+//const API_BASE_URL = "http://localhost:3000/api/workexperience"; 
+const API_BASE_URL = "https://moment2-cv-backend.onrender.com";
